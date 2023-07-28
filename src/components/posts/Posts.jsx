f0a242db7eaa9922221ede49f60bd42908a45c88
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2023-07-28 19:44:24 +0200

    Update Posts.jsx
    updated img , from pexels.com

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,11 +4,11 @@ import "./posts.css";
 export default function Posts() {
   return (
     <div className="posts">
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXtSBZ1wNn_3lN_Dq4syzs7V6D93wLImCiMQ&usqp=CAU"/>
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5MqXlwft0BRQyaweoywB4R20CUewf0XkZ2A&usqp=CAU"/>
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXtSBZ1wNn_3lN_Dq4syzs7V6D93wLImCiMQ&usqp=CAU"/>
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5MqXlwft0BRQyaweoywB4R20CUewf0XkZ2A&usqp=CAU"/>
-      <Post img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXtSBZ1wNn_3lN_Dq4syzs7V6D93wLImCiMQ&usqp=CAU"/>
+      <Post img="https://images.pexels.com/photos/16831012/pexels-photo-16831012/free-photo-of-pigeons-in-air.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
+      <Post img="https://images.pexels.com/photos/16865475/pexels-photo-16865475/free-photo-of-city-building-office-architecture.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
+      <Post img="https://images.pexels.com/photos/15469264/pexels-photo-15469264/free-photo-of-sea-water-wave-ocean.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
+      <Post img="https://images.pexels.com/photos/17300131/pexels-photo-17300131/free-photo-of-laguna-halcon-ushuaia-tierra-del-fuego-argentina.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
+      <Post img="https://images.pexels.com/photos/16542274/pexels-photo-16542274/free-photo-of-silhouette-at-golden-sunrise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
     </div>
   );
 }
